c94c2a3b82e300677e7310fc10bba30e7b67c8fe
HAMISH <constatcopywriting@gmail.com>
2018-03-27 20:57:51 +1300

    Adjusted shadow width and crescent height for portrat mode. Too complex - about to refactor weights to planetsize instead of screensize so that screen mode does not need to be taken into account.

--- src/components/Planet.js
+++ src/components/Planet.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react'
 import earth from '../assets/images/earth.gif'
 
+//weight everything to planet width instead of screenwidth?
+
 export default class Planet extends Component {
   constructor(props) {
     super(props)
 
     this.horizonShine = this.horizonShine.bind(this)
     this.shadowSpread = this.shadowSpread.bind(this)
+    this.shadowWidth = this.shadowWidth.bind(this)
     this.crescentHeight = this.crescentHeight.bind(this)
     this.planetStyleProperties = this.planetStyleProperties.bind(this)
     this.planetSize = this.planetSize.bind(this)
@@ -14,34 +17,29 @@ export default class Planet extends Component {
 
   horizonShine() {
     const { scrolled } = this.props
-    const peakShine = 3.5
-    const shineFloor = 5
+    const peakShine = 5.8 //scroll point at which blur-radius starts decreasing
+    const deltaFloor = 8  //scroll point at which blur-radius stops decreasing
+    const minimumShine = -5
 
-    if (scrolled > shineFloor) return -4
+    if (scrolled > deltaFloor) return minimumShine
+
     return scrolled < peakShine ?
-      scrolled * -2 : (peakShine - (scrolled - peakShine)) * -2
-  }
-
-  shadowSpread() {
-    const { scrolled, width } = this.props
-    const standardScreenWidth = 1034
-    const blurRadius = ((15 + (scrolled / 5)) / standardScreenWidth) * width
-
-    return `0px -5px
-            ${blurRadius}px
-            ${10 + scrolled/5}px
-            rgb(35, 35, 35)`
+      scrolled * -1.3 : (peakShine - (scrolled - peakShine)) * -1.3
   }
 
   crescentHeight() {
-    const { scrolled, width } = this.props
+    const { scrolled, width, isPortrait } = this.props
     const standardScreenWidth = 1034
     const maximumHeight = 45
-    const screenWeightedHeight = 20 +
-      ((scrolled * 1.95) / standardScreenWidth) * width
 
-    return screenWeightedHeight > maximumHeight ?
-      maximumHeight : screenWeightedHeight
+    const weightedByScreenOrientationMode = isPortrait ?
+      scrolled * 4 : scrolled * 1.95
+
+    const screenWidthWeightedHeight = 20 +
+      (weightedByScreenOrientationMode / standardScreenWidth) * width
+
+    return screenWidthWeightedHeight > maximumHeight ?
+      maximumHeight : screenWidthWeightedHeight
   }
 
   planetSize() {
@@ -56,6 +54,28 @@ export default class Planet extends Component {
     return scrolled < startZoom ? screenMode : screenMode + acceleration
   }
 
+  shadowSpread() {
+    const { scrolled, width, isPortrait } = this.props
+    const standardScreenWidth = 1034
+    const blurRadius = ((15 + (scrolled / 5)) / standardScreenWidth) * width
+
+    return `0px -5px
+            ${blurRadius}px
+            ${10 + scrolled/5}px
+            rgb(35, 35, 35)`
+  }
+
+  //possibly remove
+  shadowWidth() {
+    const { scrolled, isPortrait } = this.props
+    const planetSize = this.planetSize()
+
+    const width = isPortrait ?
+      planetSize : planetSize + (scrolled * 1.5)
+
+    return `${width}px`
+  }
+
   planetStyleProperties() {
     const { scrolled } = this.props
     return {
@@ -65,6 +85,7 @@ export default class Planet extends Component {
       planetOverlayColor: 'rgba(109, 145, 145, 0.65)',
       shadowColor: 'rgb(35, 35, 35)',
       shadowSpread: this.shadowSpread(),
+      shadowWidth: this.shadowWidth(),
       crescentHeight: this.crescentHeight(),
       planetSize: this.planetSize()
     }
@@ -79,6 +100,7 @@ export default class Planet extends Component {
       planetOverlayColor,
       shadowColor,
       shadowSpread,
+      shadowWidth,
       crescentHeight,
       planetSize
     } = this.planetStyleProperties()
@@ -109,7 +131,7 @@ export default class Planet extends Component {
         <div style={{
           position: 'absolute',
           background: shadowColor,
-          width: `${(planetSize + 10) + (scrolled * 1.8)}px`,
+          width: shadowWidth,
           height: `${planetSize}px`,
           borderRadius: '50%',
           marginTop: `${crescentHeight}px`,
