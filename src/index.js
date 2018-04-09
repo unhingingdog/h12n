a741a90c1bf975d7e02b154dc84059865517e606
HAMISH <constatcopywriting@gmail.com>
2018-04-09 16:29:34 +1200

    reset tracked files (I think)

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './components/App.js'
-//sdfafasdfsfsdafsadf
+//sdfafasdfsfsdafsadfjhgjh
 
 ReactDOM.render(
     <App />, document.getElementById('root')
